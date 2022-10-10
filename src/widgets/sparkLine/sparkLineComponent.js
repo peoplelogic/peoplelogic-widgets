50abeccbfa334117830ab80b7b8d050eb9da567a
webCreativeDev <joaoaniceto.webdeveloper@gmail.com>
2022-10-10 16:41:07 +0100

    adjusting height of sparkLine

--- src/widgets/sparkLine/sparkLineComponent.js
+++ src/widgets/sparkLine/sparkLineComponent.js
@@ -1,10 +1,10 @@
 import React,{useEffect} from 'react';
 import {setSparkLine} from './setSparkLine';
 
-const SparkLineSvg = ({width,height,strokeWidth,percentages}) =>{
+const SparkLineSvg = ({width,strokeWidth,percentages}) =>{
 
   useEffect(() => {
-    setSparkLine(width,height,strokeWidth,percentages);
+    setSparkLine(width,strokeWidth,percentages);
   }, []);
   
 
